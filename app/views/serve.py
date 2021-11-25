b39a1de370e62dfde705e614fed848be1bc5fe8e
JacobG03 <jacobgluszek03@gmail.com>
2021-11-25 20:10:58 +0100

    fix

--- app/views/serve.py
+++ app/views/serve.py
@@ -1,17 +1,18 @@
 from flask import Blueprint, send_from_directory
 import os
+from app import app
 
 """
 This code is responsible for serving a react app ("client").
 """
 
-serve = Blueprint('serve', __name__, static_folder=os.path.abspath('../../client/build'), static_url_path='/')
+serve = Blueprint('serve', __name__)
 
 @serve.route('/', defaults={'path': ''})
 @serve.route('/<path:path>')
 def catch_all(path):
-    return send_from_directory(serve.static_folder, 'index.html')
+    return send_from_directory(app.static_folder, 'index.html')
 
 @serve.errorhandler(404)   
 def not_found(e):   
-  return send_from_directory(serve.static_folder, 'index.html')
+  return send_from_directory(app.static_folder, 'index.html')
